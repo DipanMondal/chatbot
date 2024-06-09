e4de46e1844e07dec5f3dbbd6d35d31607bb4381
Dipan31Mondal <idipan2003@gmail.com>
2024-06-09 10:45:09 +0530

    Larger vocab with punctuations ans contraction handling

--- functions.py
+++ functions.py
@@ -47,3 +47,28 @@ def preprocess_input(input_sentence):
     seq = Sent2Seq(input_sentence)
     padding(seq,qs_max)
     return np.array(seq)
+    
+def QandA(enc_model,dec_model,vocabulary,preprocess_input,sentence):
+    states_values = enc_model.predict(np.array([preprocess_input(sentence)]))
+    empty_target_seq = np.zeros((1 , 1))
+    empty_target_seq[0, 0] = vocab['<start>']
+    stop_condition = False
+    decoded_translation = ''
+    
+    while not stop_condition :
+        dec_outputs , h , c = dec_model.predict([empty_target_seq] + states_values)
+        sampled_word_index = np.argmax(dec_outputs[0, -1, :])
+        sampled_word = None
+        
+        word = vocabulary[sampled_word_index]
+        decoded_translation += f' {word}'
+        sampled_word = word
+        
+        if sampled_word == '<end>' or len(decoded_translation.split()) > ans_max:
+            stop_condition = True
+            
+        empty_target_seq = np.zeros((1 , 1))  
+        empty_target_seq[0 , 0] = sampled_word_index
+        states_values = [h , c] 
+    ans = decoded_translation.replace("<end>","")
+    return ans
